3f475f7af99576f9f2a2ce9a28622821233653ff
Piotr Zagdanski <piotr.zagdanski@restore.eu>
2019-03-11 22:31:28 +0100

    solution

--- scala/src/main/scala/com/gildedrose/Item.scala
+++ scala/src/main/scala/com/gildedrose/Item.scala
@@ -2,4 +2,31 @@ package com.gildedrose
 
 class Item(val name: String, var sellIn: Int, var quality: Int) {
 
+  def updateQuality() {
+    name match {
+
+      case "Aged Brie" =>
+        sellIn = sellIn -1
+        quality = quality + (if (sellIn < 0) 2 else 1)
+        quality = math.min(50, quality)
+
+      case "Sulfuras, Hand of Ragnaros" =>
+
+      case "Backstage passes to a TAFKAL80ETC concert" =>
+        sellIn = sellIn - 1
+        quality = quality + (sellIn match {
+          case s: Int if s >= 10 => 1
+          case s: Int if s >= 5 => 2
+          case s: Int if s >= 0 => 3
+          case _ => -1 * quality
+        })
+        quality = math.min(50, quality)
+
+      case _ =>
+        sellIn = sellIn - 1
+        quality = math.max(0, quality - (if (sellIn < 0) 2 else 1))
+
+    }
+  }
+
 }
--- scala/src/test/scala/com/gildedrose/GildedRoseTest.scala
+++ scala/src/test/scala/com/gildedrose/GildedRoseTest.scala
@@ -2,11 +2,120 @@ package com.gildedrose
 
 import org.scalatest._
 
-class GildedRoseTest  extends FlatSpec with Matchers {
-      it should "foo" in {
-        var items = Array[Item](new Item("foo", 0, 0))
-        val app = new GildedRose(items)
-        app.updateQuality()
-        (app.items(0).name) should equal ("fixme")
-      }
+class GildedRoseSpec extends FlatSpec with Matchers {
+
+  behavior of "GildedRose"
+
+  it should "properly update normal item" in new Fixture {
+    var items = Array[Item](normal)
+    val app = new GildedRose(items)
+    app.updateQuality()
+    app.items(0).name should equal("+5 Dexterity Vest")
+    app.items(0).sellIn should equal(9)
+    app.items(0).quality should equal(19)
+  }
+
+  it should "properly update normal item after sellIn degrades" in new Fixture {
+    var items = Array[Item](normal)
+    val app = new GildedRose(items)
+    for (_ <- 1 to 12) { app.updateQuality() }
+    app.items(0).name should equal("+5 Dexterity Vest")
+    app.items(0).sellIn should equal(-2)
+    app.items(0).quality should equal(6)
+  }
+
+  it should "properly update Brie" in new Fixture {
+    var items = Array[Item](brie)
+    val app = new GildedRose(items)
+    app.updateQuality()
+    app.items(0).name should equal("Aged Brie")
+    app.items(0).sellIn should equal(1)
+    app.items(0).quality should equal(1)
+  }
+
+  it should "properly update Brie after sellIn degrades" in new Fixture {
+    var items = Array[Item](brie)
+    val app = new GildedRose(items)
+    for (_ <- 1 to 6) { app.updateQuality() }
+    app.items(0).name should equal("Aged Brie")
+    app.items(0).sellIn should equal(-4)
+    app.items(0).quality should equal(10)
+  }
+
+  it should "properly update Sulfuras" in new Fixture {
+    var items = Array[Item](sulfuras)
+    val app = new GildedRose(items)
+    app.updateQuality()
+    app.items(0).name should equal("Sulfuras, Hand of Ragnaros")
+    app.items(0).sellIn should equal(5)
+    app.items(0).quality should equal(80)
+  }
+
+  it should "properly update Sulfuras after sellIn degrades" in new Fixture {
+    var items = Array[Item](sulfuras)
+    val app = new GildedRose(items)
+    for (_ <- 1 to 6) { app.updateQuality() }
+    app.items(0).name should equal("Sulfuras, Hand of Ragnaros")
+    app.items(0).sellIn should equal(5)
+    app.items(0).quality should equal(80)
+  }
+
+  it should "properly update Backstage pass" in new Fixture {
+    var items = Array[Item](backstagePass)
+    val app = new GildedRose(items)
+    app.updateQuality()
+    app.items(0).name should equal("Backstage passes to a TAFKAL80ETC concert")
+    app.items(0).sellIn should equal(11)
+    app.items(0).quality should equal(21)
+  }
+
+  it should "properly update Backstage pass when  5 < sellin <= 10 " in new Fixture {
+    var items = Array[Item](backstagePass)
+    val app = new GildedRose(items)
+    for (_ <- 1 to 3) { app.updateQuality() }
+    app.items(0).name should equal("Backstage passes to a TAFKAL80ETC concert")
+    app.items(0).sellIn should equal(9)
+    app.items(0).quality should equal(24)
+  }
+
+  it should "properly update Backstage pass when  sellin <= 5 " in new Fixture {
+    var items = Array[Item](backstagePass)
+    val app = new GildedRose(items)
+    for (_ <- 1 to 8) { app.updateQuality() }
+    app.items(0).name should equal("Backstage passes to a TAFKAL80ETC concert")
+    app.items(0).sellIn should equal(4)
+    app.items(0).quality should equal(35)
+  }
+
+  it should "properly update Backstage pass when sellin <0  " in new Fixture {
+    var items = Array[Item](backstagePass)
+    val app = new GildedRose(items)
+    for (_ <- 1 to 13) { app.updateQuality() }
+    app.items(0).name should equal("Backstage passes to a TAFKAL80ETC concert")
+    app.items(0).sellIn should equal(-1)
+    app.items(0).quality should equal(0)
+  }
+
+  it should "Once the sell by date has passed, Quality degrades twice as fast" in new Fixture {
+    var items = Array[Item](normal)
+    val app = new GildedRose(items)
+    for (_ <- 1 to 10) { app.updateQuality() }
+    app.items(0).quality should equal(10)
+    app.updateQuality()
+    app.items(0).quality should equal(8)
+  }
+
+  it should "The Quality of an item is never negative" in new Fixture {
+    var items = Array[Item](normal)
+    val app = new GildedRose(items)
+    for (_ <- 1 to 25) { app.updateQuality() }
+    app.items(0).quality should equal(0)
+  }
+
+  class Fixture {
+    val normal = new Item("+5 Dexterity Vest", 10, 20)
+    val brie = new Item("Aged Brie", 2, 0)
+    val sulfuras = new Item("Sulfuras, Hand of Ragnaros", 5, 80)
+    val backstagePass = new Item("Backstage passes to a TAFKAL80ETC concert", 12, 20)
+  }
 }
